8563372cefe986c26332726e29b904e1db05ca51
sahmiran <hasancantural19@gmail.com>
2022-06-04 21:57:18 +0300

    Register with db

--- OOP_II_Game/OOP_II_Game/UserRepository.cs
+++ OOP_II_Game/OOP_II_Game/UserRepository.cs
@@ -15,10 +15,10 @@ namespace OOP_II_Game
         {
             using(IDbConnection db = new SqlConnection(AppHelper.ConnectionString)) 
             {
-                var result = await db.ExecuteAsync(OOP_II_Game.Properties.Resources.InsertUser, new { username = user.Username , 
-                                                                                                        password = user.Password , fullname = user.Fullname , 
-                                                                                                        phonenumber = user.PhoneNumber , address = user.Address , 
-                                                                                                        city = user.City , country = user.Country , email = user.Email });
+                var result = await db.ExecuteAsync(OOP_II_Game.Properties.Resources.InsertUser, new { Username = user.Username , 
+                                                                                                        Password = user.Password , Fullname = user.Fullname , 
+                                                                                                        PhoneNumber = user.PhoneNumber , Address = user.Address , 
+                                                                                                        City = user.City , Country = user.Country , Email = user.Email });
                 return result>0;
                 //Sql tanimlamasini resource kismina ekledik degisiklik yapmasi daha kolay olmasi icin
             }
