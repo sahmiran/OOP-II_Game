987bd174d1d8d8b6b827805feaf88a3d0dfcb35a
sahmiran <hasancantural19@gmail.com>
2022-06-04 20:21:56 +0300

    deneme

---
+++ OOP_II_Game/OOP_II_Game/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace OOP_II_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textNameSurname = new System.Windows.Forms.TextBox();
+            this.textPhoneNumber = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textCity = new System.Windows.Forms.TextBox();
+            this.textCountry = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(600, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(526, 341);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Log In";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.buttonRegister);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textEmail);
+            this.tabPage2.Controls.Add(this.textCountry);
+            this.tabPage2.Controls.Add(this.textCity);
+            this.tabPage2.Controls.Add(this.textAddress);
+            this.tabPage2.Controls.Add(this.textPhoneNumber);
+            this.tabPage2.Controls.Add(this.textNameSurname);
+            this.tabPage2.Controls.Add(this.textPassword);
+            this.tabPage2.Controls.Add(this.textUsername);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(592, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sign In";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // textUsername
+            // 
+            this.textUsername.Location = new System.Drawing.Point(157, 32);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(235, 22);
+            this.textUsername.TabIndex = 0;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(157, 60);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(235, 22);
+            this.textPassword.TabIndex = 1;
+            // 
+            // textNameSurname
+            // 
+            this.textNameSurname.Location = new System.Drawing.Point(157, 88);
+            this.textNameSurname.Name = "textNameSurname";
+            this.textNameSurname.Size = new System.Drawing.Size(235, 22);
+            this.textNameSurname.TabIndex = 2;
+            // 
+            // textPhoneNumber
+            // 
+            this.textPhoneNumber.Location = new System.Drawing.Point(157, 116);
+            this.textPhoneNumber.Name = "textPhoneNumber";
+            this.textPhoneNumber.Size = new System.Drawing.Size(235, 22);
+            this.textPhoneNumber.TabIndex = 3;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(157, 144);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(235, 22);
+            this.textAddress.TabIndex = 4;
+            // 
+            // textCity
+            // 
+            this.textCity.Location = new System.Drawing.Point(157, 172);
+            this.textCity.Name = "textCity";
+            this.textCity.Size = new System.Drawing.Size(235, 22);
+            this.textCity.TabIndex = 5;
+            // 
+            // textCountry
+            // 
+            this.textCountry.Location = new System.Drawing.Point(157, 200);
+            this.textCountry.Name = "textCountry";
+            this.textCountry.Size = new System.Drawing.Size(235, 22);
+            this.textCountry.TabIndex = 6;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(157, 228);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(235, 22);
+            this.textEmail.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Name-Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Phone Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "City";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Country";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 234);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "E-mail";
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(278, 256);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(114, 50);
+            this.buttonRegister.TabIndex = 16;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textCountry;
+        private System.Windows.Forms.TextBox textCity;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textPhoneNumber;
+        private System.Windows.Forms.TextBox textNameSurname;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OOP_II_Game/OOP_II_Game/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace OOP_II_Game
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task<bool> Insert(User user)
+        {
+            using(IDbConnection db = new SqlConnection(AppHelper.ConnectionString)) 
+            {
+                var result = await db.ExecuteAsync(OOP_II_Game.Properties.Resources.InsertUser, new { username = user.Username , 
+                                                                                                        password = user.Password , fullname = user.Fullname , 
+                                                                                                        phonenumber = user.PhoneNumber , address = user.Address , 
+                                                                                                        city = user.City , country = user.Country , email = user.Email });
+                return result>0;
+                //Sql tanimlamasini resource kismina ekledik degisiklik yapmasi daha kolay olmasi icin
+            }
+        }
+    }
+}
